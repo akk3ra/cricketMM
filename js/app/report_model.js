5f79d3fbf2fbcf8d5bf65240ce9874a8a7f5bf73
Sandeep <a.to_s@yahoo.com>
2014-03-26 10:43:05 -0500

    Made changes to the report fixtures..

--- js/app/report_model.js
+++ js/app/report_model.js
@@ -8,7 +8,7 @@ Cricket.Report = Ember.Model.extend({
 	matchTime: DS.attr('date'),
 	venue: DS.attr('string'),
 	stats: DS.attr('stats'),
-	computeReadableTime: function(){
+	readableTime: function(){
 			// Code to make the time into readable format
 			return Moment(this.get('matchTime')).fromTime();
 
@@ -19,10 +19,10 @@ Cricket.Report = Ember.Model.extend({
 
 Cricket.Stats = Ember.Object.extend({
 	id: DS.attr('string'),
-	teamA: DS.hasMany('mainStat'),
-	teamB: DS.hasMany('mainStat'),
 	matchResult: DS.attr('string'),
-	matchComplete: DS.attr('string')
+	matchComplete: DS.attr('string'),
+	teamA: DS.hasMany('mainStat'),
+	teamB: DS.hasMany('mainStat')
 });
 
 Cricket.MainStat = Ember.Object.extend({
@@ -32,9 +32,30 @@ Cricket.MainStat = Ember.Object.extend({
 	batStat: DS.hasMany('batStat')
 });
 
-Cricket,BatStat = Ember.Object.extend({
-
+Cricket.BatStat = Ember.Object.extend({
+	bowlerId: DS.attr('string'),
+	bowlerName: DS.attr('string'),
+	strikes: DS.hasMany('currContext')
 	
+});
+
+Cricket.CurrContext = Ember.Object.extend({
+	id: DS.attr('int'),
+	deliveryType: DS.attr('string'),
+	batsmanResponse: DS.attr('string'),
+	runsScored: DS.attr('int'),
+	shotStyle: DS.attr('string')
 });
 Cricket.Report.FIXTURES = [
 ]
+
+
+
+
+
+
+
+
+
+
+
